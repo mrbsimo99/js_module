7b44f3bb93fb94627f221d79e8e4de434a8c8f27
Simone <morabitosimone99@gmail.com>
2025-05-05 14:49:43 +0200

    completed 10th ex

--- ex_if&else/script.js
+++ ex_if&else/script.js
@@ -1,1 +1,9 @@
 // Esercizio 10 If & else
+let number = 3;
+
+if (number > 5) {
+   console.log("La variabile è maggiore di 5")
+  } else {
+      console.log("La variabile è minore di 5") 
+    }
+
