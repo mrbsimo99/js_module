4edea57522477ba8750c6a48e6cf9817e691ed50
Simone <morabitosimone99@gmail.com>
2025-05-06 08:34:32 +0200

    completed

--- ex_if-else&else-if/script.js
+++ ex_if-else&else-if/script.js
@@ -1,17 +1,14 @@
 // Esercizio 11 - if,else & else-if
 let score = parseInt(prompt('Inserisci il tuo voto'));
 
-if (!isNaN(score) && score >= 0 && score <= 100) {
-  if (score >= 90 && score <= 100) {
-    console.log("voto ottimo");
-  } else if (score >= 70 && score <= 89) {
-    console.log("voto buono");
-  } else if (score >= 60 && score <= 69) {
-    console.log("voto sufficiente");
-  } else {
-    console.log("voto insufficiente");
-  }
-} 
-else {
-  console.log("Valore non valido");
+if (score >= 90 && score <= 100) {
+  console.log("voto ottimo");
+} else if (score >= 70 && score <= 89) {
+  console.log("voto buono");
+} else if (score >= 60 && score <= 69) {
+  console.log("voto sufficiente");
+} else if (score <= 59) {
+  console.log("voto insufficiente");
+} else {
+  console.log("Punteggio non valido");
 }
