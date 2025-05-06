6cf45c89aafffabe62312fb873fd246652e7bebe
Simone <morabitosimone99@gmail.com>
2025-05-06 08:47:24 +0200

    completed switch exercise

---
+++ ex_switch/script.js
@@ -0,0 +1,14 @@
+// Esercizio 11 - if,else & else-if
+let score = parseInt(prompt('Inserisci il tuo voto'));
+
+if (score >= 90 && score <= 100) {
+  console.log("voto ottimo");
+} else if (score >= 70 && score <= 89) {
+  console.log("voto buono");
+} else if (score >= 60 && score <= 69) {
+  console.log("voto sufficiente");
+} else if (score <= 59) {
+  console.log("voto insufficiente");
+} else {
+  console.log("Punteggio non valido");
+}
