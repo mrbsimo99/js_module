5b6896dee7da136252ebe6dadeeba7722e19b4c9
Simone <morabitosimone99@gmail.com>
2025-05-06 08:50:33 +0200

    renamed folders

--- ex_if-else&else-if/script.js
+++ ex_if-else&else-if/script.js
@@ -1,19 +1,14 @@
-// Esercizio 12 - if,else & else-if
-let score = parseInt(prompt("Inserisci il tuo voto"));
+// Esercizio 12 - switch
+let score = parseInt(prompt('Inserisci il tuo voto'));
 
-switch (true) {
-  case score >= 90 && score <= 100:
-    console.log("voto ottimo");
-    break;
-  case score >= 70 && score <= 89:
-    console.log("voto buono");
-    break;
-  case score >= 60 && score <= 69:
-    console.log("voto sufficiente");
-    break;
-  case score <= 59:
-    console.log("voto insufficiente");
-    break;
-  default:
-    console.log("voto non valido");
+if (score >= 90 && score <= 100) {
+  console.log("voto ottimo");
+} else if (score >= 70 && score <= 89) {
+  console.log("voto buono");
+} else if (score >= 60 && score <= 69) {
+  console.log("voto sufficiente");
+} else if (score <= 59) {
+  console.log("voto insufficiente");
+} else {
+  console.log("Punteggio non valido");
 }
