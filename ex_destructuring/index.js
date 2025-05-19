c3f7549620c70bf3ec264853678d6b427a97e140
Simone Morabito <simonemorabito@Mini-di-Simone.homenet.telecomitalia.it>
2025-05-19 11:59:16 +0200

    completed 2 exercises

---
+++ ex_destructuring/index.js
@@ -0,0 +1,14 @@
+// Estrazione elementi base 
+
+let numeri = [1 , 3, 5, 9, 12, 18]; 
+
+let [primo, secondo, terzo, quarto, quinto, sesto] = numeri;
+
+console.log(primo, secondo);
+
+// Saltare elementi durante il destructuring 
+
+console.log(primo,terzo)
+
+// Destructuring con valori predefiniti
+
