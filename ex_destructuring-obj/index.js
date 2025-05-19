2f791d44b111f9db7870061dce92487605b8b666
Simone Morabito <simonemorabito@Mini-di-Simone.homenet.telecomitalia.it>
2025-05-19 13:26:12 +0200

    completed 9th ex

--- ex_destructuring-obj/index.js
+++ ex_destructuring-obj/index.js
@@ -1,43 +1,51 @@
 // Estrazione di proprietà base
 
 let Pokèmon = {
-    nome: "Charizard",
-    tipo: "Fuoco, Volante",
-    mossa: "Lanciafiamme",
-    debolezza: "Acqua, Elettro",
+  nome: "Charizard",
+  tipo: "Fuoco, Volante",
+  mossa: "Lanciafiamme",
+  debolezza: "Acqua, Elettro",
 };
 
 let { nome, tipo, mossa, debolezza } = Pokèmon;
 
 console.log(
-    "nome:",
-    nome,
-    "tipo:",
-    tipo,
-    "mossa:",
-    mossa,
-    "debolezza:",
-    debolezza
+  "nome:",
+  nome,
+  "tipo:",
+  tipo,
+  "mossa:",
+  mossa,
+  "debolezza:",
+  debolezza
 );
 
 // Destructuring con nomi di variabili diversi
 
-let {nome : prima, tipo : seconda, mossa : terza, debolezza : quarta} = Pokèmon
+let { nome: prima, tipo: seconda, mossa: terza, debolezza: quarta } = Pokèmon;
 
-console.log(prima,seconda,terza,quarta)
-
+console.log(prima, seconda, terza, quarta);
 
 // Destructuring con valori predefiniti
 
 let Automobile = {
-    marca:"Alfa", 
-    modello:"Mto", 
-}
+  marca: "Alfa",
+  modello: "Mto",
+};
 
-let {marca, modello, anno= 0, chilometraggio= 0} = Automobile;
+let { marca, modello, anno = 0, chilometraggio = 0 } = Automobile;
 
 anno = 2011;
 chilometraggio = 200000;
 
-console.log(marca, modello, anno, chilometraggio)
+console.log(marca, modello, anno, chilometraggio);
 
+// Copia di un oggetto con proprietà aggiuntive
+
+let PokèmonStarter = {
+  ...Pokèmon,
+  altezza: "1,7m",
+  peso: "90,5 kg"
+};
+
+console.log(PokèmonStarter);
