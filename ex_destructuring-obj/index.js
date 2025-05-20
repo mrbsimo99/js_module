127f734d9221a57991a607510ad3dbcd192ba049
Simone Morabito <simonemorabito@Mini-di-Simone.homenet.telecomitalia.it>
2025-05-20 12:32:06 +0200

    completed ex

--- ex_destructuring-obj/index.js
+++ ex_destructuring-obj/index.js
@@ -45,7 +45,82 @@ console.log(marca, modello, anno, chilometraggio);
 let PokèmonStarter = {
   ...Pokèmon,
   altezza: "1,7m",
-  peso: "90,5 kg"
+  peso: "90,5 kg",
 };
 
 console.log(PokèmonStarter);
+
+// Funzione con parametri variabili
+function somma(...Valori) {
+  return Valori.reduce((acc, cur) => acc + cur, 0);
+}
+console.log(somma(1, 3, 5, 11, 15, 23));
+
+// Destructuring oggetto con rest operator
+
+const Persona = {
+  nome1: "Giuseppe",
+  cognome: "Rossi",
+  citta: "Roma",
+  eta: 30,
+};
+
+const { nome1, cognome, ...altro } = Persona;
+
+console.log(nome1);
+console.log(cognome);
+console.log(altro);
+
+// Conversione di un oggetto in una stringa JSON
+
+const PersonaInJson = JSON.stringify(Persona);
+console.log(PersonaInJson);
+
+// Conversione di una stringa JSON in un oggetto
+
+const Json =
+  '{"nome": "Franco", "cognome": "Verdi", "citta": "Milano", "eta": "50"}';
+const JsonPersona = JSON.parse(Json);
+console.log(JsonPersona);
+
+// Manipolazione di dati JSON
+
+const JsonListaDiPersone = `[
+  { "nome": "Giuseppe", "citta": "Palermo"},
+  { "nome": "Francesco", "citta": "Reggio Calabria"},
+  { "nome": "Giovanni", "citta": "Potenza"}
+]`;
+
+const ListaDiPersone = JSON.parse(JsonListaDiPersone)
+console.log(ListaDiPersone);
+
+ListaDiPersone.push({
+  nome: "Marco",
+  citta: "Messina",
+});
+
+const ListaDiPersoneAggiornata = JSON.stringify(ListaDiPersone);
+console.log(ListaDiPersoneAggiornata);
+
+// Stringa semplice con template literals
+
+let nome2 = "Alessandro";
+let cognome2 = "Magno";
+
+let stringaSemplice = `${nome2} ${cognome}`;
+
+console.log(stringaSemplice)
+
+// Stringa multilinea con template literals
+
+let nome3 = "Paolo";
+let cognome3 = "Strati";
+let eta3 = 36;
+let citta3 = "Benevento";
+
+let StringaMultilinea = `nome: ${nome3} 
+cognome: ${cognome3} 
+età: ${eta3}  
+città: ${citta3}`
+
+console.log(StringaMultilinea)
