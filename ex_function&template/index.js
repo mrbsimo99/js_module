80795472c5b8847feb267addbeeb1c80d54f89bb
Simone <morabitosimone99@gmail.com>
2025-05-20 15:09:21 +0200

    completed ex

--- ex_function&template/index.js
+++ ex_function&template/index.js
@@ -5,7 +5,7 @@ let cognome2 = "Magno";
 
 let stringaSemplice = `${nome2} ${cognome2}`;
 
-console.log(stringaSemplice)
+console.log(stringaSemplice);
 
 // Stringa multilinea con template literals
 
@@ -17,6 +17,32 @@ let citta3 = "Benevento";
 let StringaMultilinea = `nome: ${nome3} 
 cognome: ${cognome3} 
 età: ${eta3}  
-città: ${citta3}`
+città: ${citta3}`;
 
-console.log(StringaMultilinea)
+console.log(StringaMultilinea);
+
+// Funzione e template literals
+function Persona4(persona) {
+  return `
+  nome: ${persona.nome}
+  cognome: ${persona.cognome}
+  eta: ${persona.eta}
+  citta: ${persona.citta}
+  `;
+}
+
+const QuartaPersona = {
+  nome: "Claudia",
+  cognome: "Spadino",
+  eta: 27,
+  citta: "Roma"
+};
+
+console.log(Persona4(QuartaPersona));
+
+// Utilizzare console log
+
+console.log(10, "Ciao", [2, 4, 6]);
+
+let mioOggetto = {marca: "Alfa", modello: "Mito"};
+console.log(mioOggetto)
